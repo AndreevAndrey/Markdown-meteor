2bc5d6274cced81ba336d668938999c6471b32aa
AndreevAndrey <888Andrei888@mail.ru>
2020-11-06 14:31:11 +0300

    Remade editPanel, added link, list, subtitle in icons

---
+++ imports/ui/Editor/EditPanel/Icons/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const List = () => (
+  <svg viewBox="0 0 231 231" enableBackground="new 0 0 231 231">
+    <g>
+      <rect width="181" x="50" y="164.5" height="33"/>
+      <rect width="181" x="50" y="99.5" height="33"/>
+      <rect width="181" x="50" y="32.5" height="33"/>
+      <rect width="33" y="165.5" height="33"/>
+      <rect width="33" y="99.5" height="33"/>
+      <rect width="33" y="32.5" height="33"/>
+    </g>
+  </svg>
+);
